957143c2e8513c86facb1e736a82ed1d5bb650e0
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-02-23 20:28:27 +0100

    birthday reminder...

---
+++ 1-birthday-reminder/birthday-reminder/src/App.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react';
+// import data
+import data from './data';
+// import comonents
+import List from './components/List';
+import './App.css';
+
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+     <section className="container">
+      <h3>{people.length} birthday's today</h3>
+      <List people={people}/>
+      <button onClick={() => setPeople([])}> clear all</button>
+     </section>
+    </main>
+  );
+}
+
+export default App;
